3440c5b0a6e8cca94a84a4b66281654301a95b8c
Fabio Junior <fabbhio007@gmail.com>
2024-01-31 23:53:35 -0400

    error

--- aula_tech/usuarios/views.py
+++ aula_tech/usuarios/views.py
@@ -1,15 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    if request.method == "GET":
-        return render(request,'cadastro.html')
-    else:
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        return HttpResponse(username)
+    return render(request,'cadastro.html')
 
 
 def login(request):
